3808c3bb5f86d746e7fca8b1d2c16a2da6ca379b
Xinyi Zhu <juliezhu212@outlook.com>
2022-05-26 21:43:39 -0500

    finish

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -7,16 +7,34 @@
 //
 
 import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
+    let color = (underweight: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1), healthy: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1), overweight: #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))
     
     func getBMIValue() -> String {
-        return String(format: "%.1f", bmi ?? 0.0)
+        return String(format: "%.1f", bmi?.value ?? 0.0)
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No Advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies", color: color.underweight)
+        } else if bmiValue > 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: color.overweight)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: color.healthy)
+        }
     }
 }
